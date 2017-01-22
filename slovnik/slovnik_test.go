9cd2fb22617760c233b77dd628983ba222887c10
Roman Peshkov <peshkovroman@gmail.com>
2017-01-23 00:04:30 +0100

    Added tests for page parsing

--- slovnik/slovnik_test.go
+++ slovnik/slovnik_test.go
@@ -1,6 +1,7 @@
 package slovnik
 
 import "testing"
+import "os"
 
 func TestDetectLanguage(t *testing.T) {
 	cases := []struct {
@@ -17,5 +18,26 @@ func TestDetectLanguage(t *testing.T) {
 			t.Errorf("DetectLanguage(%q) == %q, want %q", c.in, got, c.lang)
 		}
 	}
+}
 
+func TestParsePage(t *testing.T) {
+	f, _ := os.Open("./sample.html")
+	w := parsePage(f)
+
+	const expectedWord = "hlavní"
+	expectedTranslations := []string{
+		"гла́вный",
+		"основно́й",
+		"центра́льный",
+	}
+
+	if w.Word != expectedWord {
+		t.Errorf("ParsePage word == %q, want %q", w.Word, expectedWord)
+	}
+
+	for i, trans := range w.Translations {
+		if trans != expectedTranslations[i] {
+			t.Errorf("ParsePage translation == %q, want %q", trans, expectedTranslations[i])
+		}
+	}
 }
